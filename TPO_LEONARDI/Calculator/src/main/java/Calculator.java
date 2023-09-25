2647ee1ff2c4b695349154985f547cc41789c9f9
Lucas <lucas.leonardi@epfedu.fr>
2023-09-25 20:51:52 +0200

    TPO EXO 1, 2 + Partie 3 EXO 1, 2, 3

---
+++ TPO_LEONARDI/Calculator/src/main/java/Calculator.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+ /*TPO PARTIE 3 
+ * SEPTEMBRE 2023
+ *
+
+package com.mycompany.Calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leona
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter the operator: \n1) add \n2) substract \n3) multiply \n3) divide \n3) modulo");
+        int nb;
+        int nb1;
+        int nb2;
+        double resultat = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre :");
+        nb = sc.nextInt(); // On demande a sc de donner le prochain entier
+        int operateur = nb;
+        System.out.println("\n Entrer le nombre 1 :");
+        nb1 = sc.nextInt();
+        int operande1 = nb1;
+        System.out.println("\n Entrer le nombre 2 :");
+        nb2 = sc.nextInt();
+        int operande2 = nb2;
+       
+        if (operateur == 1) {
+            resultat = operande1 + operande2;
+       
+        } else if (operateur == 2) {      
+            resultat = operande1 - operande2;
+       
+        } else if (operateur == 3) {
+            resultat = operande1 * operande2;
+       
+        } else if (operateur == 4) {
+            resultat = operande1 / operande2;
+       
+        } else if (operateur == 5) {
+            resultat = operande1 % operande2;
+           
+        } else if (operateur <1 & operateur >5) {
+            System.out.println("\n Erreur, cela doit être compris entre 1 et 5");
+        }System.out.println("le résultat est :"+resultat);
+    }
+}
+
+    
