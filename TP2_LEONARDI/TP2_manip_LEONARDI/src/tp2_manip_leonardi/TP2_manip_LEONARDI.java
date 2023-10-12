6ace79c675c20291ae9872230a92544f4aeba518
Lucas <lucas.leonardi@epfedu.fr>
2023-10-12 18:24:19 +0200

    TP2 LEONARDI PARTIE 3 TERMINEE

---
+++ TP2_LEONARDI/TP2_manip_LEONARDI/src/tp2_manip_leonardi/TP2_manip_LEONARDI.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_leonardi;
+
+/**
+ *
+ * @author leona
+ */
+public class TP2_manip_LEONARDI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Tartiflette assiette1 = new Tartiflette(500) ;
+    Tartiflette assiette2 = new Tartiflette(600) ;
+    Tartiflette assiette3 = assiette2 ; 
+    
+    System.out.println("nb de calories de Assiette 2 : " + 
+    assiette2.nbCalories) ;
+    System.out.println("nb de calories de Assiette 3 : " + 
+    assiette3.nbCalories) ;
+    
+    Moussaka[] moussakaReferences = new Moussaka[10];
+    for (int i = 0; i < 10; i++) {
+        moussakaReferences[i] = new Moussaka((i + 1) * 100); // Utiliser une valeur arbitraire pour les calories
+    }
+    for (int i = 0; i < 10; i++) {
+            System.out.println("Référence " + (i + 1) + ":");
+            System.out.println("Nombre de calories : " + moussakaReferences[i].nbCalories);
+          
+    }
+    
+}
+}
