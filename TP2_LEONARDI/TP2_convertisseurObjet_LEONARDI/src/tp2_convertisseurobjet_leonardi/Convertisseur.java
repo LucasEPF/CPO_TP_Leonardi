df8b6180060dcba32be782345dba2d1da013517c
Lucas <lucas.leonardi@epfedu.fr>
2023-10-12 17:02:09 +0200

    TP2 LEONARDI PARTIE 2 TERMINEE

--- TP2_LEONARDI/TP2_convertisseurObjet_LEONARDI/src/tp2_convertisseurobjet_leonardi/Convertisseur.java
+++ TP2_LEONARDI/TP2_convertisseurObjet_LEONARDI/src/tp2_convertisseurobjet_leonardi/Convertisseur.java
@@ -13,26 +13,32 @@ public class Convertisseur {
     int nbConversions;
     
     public double CelciusVersKelvin (double tCelcius) {
+        nbConversions = nbConversions + 1;
         return tCelcius + 273.15;
     }
     
     public double KelvinVersCelcius (double tKelvin) {
+        nbConversions = nbConversions + 1;
         return tKelvin - 273.15;
     }
     
     public double FarenheitVersCelcius (double tFarenheit) {
+        nbConversions = nbConversions + 1;
         return (tFarenheit - 32) * 5/9;
     }
 
     public double CelciusVersFarenheit (double tCelcius) {
+        nbConversions = nbConversions + 1;
         return (tCelcius + 32) * 9/5;
     }
     
     public double FarenheitVersKelvin (double tFarenheit) {
+        nbConversions = nbConversions + 1;
         return (tFarenheit - 32) * 5/9 + 273.15;
     }
     
     public double KelvinVersFarenheit (double tKelvin) {
+        nbConversions = nbConversions + 1;
         return (tKelvin - 273.15) * 9/5 + 32;
     }
     
--- TP2_LEONARDI/TP2_convertisseurObjet_LEONARDI/src/tp2_convertisseurobjet_leonardi/TP2_convertisseurObjet_LEONARDI.java
+++ TP2_LEONARDI/TP2_convertisseurObjet_LEONARDI/src/tp2_convertisseurobjet_leonardi/TP2_convertisseurObjet_LEONARDI.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_leonardi;
 
+import java.util.Scanner;
+
 /**
  *
  * @author leona
@@ -15,6 +17,52 @@ public class TP2_convertisseurObjet_LEONARDI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Saississez la température : ");
+    double temp = sc.nextDouble();
+    
+    Convertisseur Convertisseur1 = new Convertisseur();
+    Convertisseur Convertisseur2 = new Convertisseur();
+    
+    double tempKelvin1 = Convertisseur1.CelciusVersKelvin(temp);
+    double tempFarenheit1 = Convertisseur1.CelciusVersFarenheit(temp);
+    
+    double Celcius2 = Convertisseur2.KelvinVersFarenheit(temp);
+    double Farenheit2 = Convertisseur2.KelvinVersFarenheit(temp);
+    
+    System.out.println(Convertisseur1);
+    System.out.println(Convertisseur2);
+    
+    
+    Convertisseur Convertisseur3 = new Convertisseur();
+    
+    double Valeur;
+    int nb1;
+    
+    
+    System.out.println("\n 1) Kelvin vers Celcius \n 2) Farenheit Vers Celcius \n 3) Celcius Vers Farenheit \n 4) Kelvin Vers Farenheit \n 5) Farenheit Vers Kelvin");
+    nb1 = sc.nextInt();
+    
+    System.out.println("\n Donner nombre réelle:");
+    Valeur = sc.nextInt(); // On demande a sc de donner le prochain entier
+    
+    if (nb1 == 1) {
+        System.out.println("La température en Celcius est :" + Convertisseur3.KelvinVersCelcius(Valeur) + " degrès");
+        
+    } else if (nb1 == 2) {
+        System.out.println("La température en Celcius est :" + Convertisseur3.FarenheitVersCelcius(Valeur) + " degrès");
+    
+    } else if (nb1 == 3) {
+        System.out.println("La température en Farenheit est :" + Convertisseur3.CelciusVersFarenheit(Valeur) + " F");
+    
+    } else if (nb1 == 4) {
+        System.out.println("La température en Farenheit est :" + Convertisseur3.KelvinVersFarenheit(Valeur) + " F");
+    
+    } else if (nb1 == 5) {
+        System.out.println("La température en Kelvin est :" + Convertisseur3.FarenheitVersCelcius(Valeur) + " Kelvin");    
     }
     
+        
+    } 
 }
+
