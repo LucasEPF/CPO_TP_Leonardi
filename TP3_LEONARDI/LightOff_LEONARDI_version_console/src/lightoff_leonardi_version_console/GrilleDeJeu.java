ded9b88aa7ff1385f6c3d9bc0c3537f707ef2585
Lucas <lucas.leonardi@epfedu.fr>
2023-10-25 18:54:39 +0200

    PROJET GUIDE

--- TP3_LEONARDI/LightOff_LEONARDI_version_console/src/lightoff_leonardi_version_console/GrilleDeJeu.java
+++ TP3_LEONARDI/LightOff_LEONARDI_version_console/src/lightoff_leonardi_version_console/GrilleDeJeu.java
@@ -4,22 +4,58 @@
  */
 package lightoff_leonardi_version_console;
 
+import static java.lang.Math.random;
+import static java.lang.StrictMath.random;
+
 /**
  *
  * @author leona
  */
 public class GrilleDeJeu {
-    
+   
    int nbLignes;
    int nbColonnes;
-   public CelluleLumineuse [][] matricesCellules;
+   CelluleLumineuse [][] matricesCellules;
+   int nombreAleatoire = random.nextInt(4) + 1;
 
     public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
         nbLignes = p_nbLignes;
         nbColonnes = p_nbColonnes;
-        
+        for (int i=0; i<nbLignes; i++) {
+            for (int j=0; j<nbColonnes;j++) {
+               matricesCellules [i][j] = new CelluleLumineuse();
+               
+            }
+            
+        }
     }
-   
-   
+public void eteindreToutesLesCellules() {
+        for (int i=0; i<nbLignes; i++) {
+            for (int j=0; j<nbColonnes;j++) {
+               matricesCellules [i][j].eteindreCellule();
+            }              
+    }
+}
+public void activerLigneDeCellules(int idLigne) {
+    matricesCellules [idLigne][0].activerCellule();
+} 
+public void activerColonneDeCellules(int idColonne) {
+    matricesCellules [0][idColonne].activerCellule();
+}  
+public void activerDiagonaleDescendante() {
+    int min = Math.min(nbLignes, nbColonnes);
+    for (int i=0; i<min; i++) {
+            matricesCellules [i][i].activerCellule();  
+}
+}
+public void activerDiagonaleMontante() {
+    int min = Math.min(nbLignes, nbColonnes);
+    for (int i=0; i<min; i++) {
+            matricesCellules [i][nbColonnes-1-i].activerCellule();
+}
+}
+public void activerLigneColonneOuDiagonaleAleatoire() {
     
 }
+}
+
---
+++ TP3_LEONARDI/LightOff_LEONARDI_version_console/src/lightoff_leonardi_version_console/LightOff_LEONARDI_version_console.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_leonardi_version_console;
+
+/**
+ *
+ * @author leona
+ */
+public class LightOff_LEONARDI_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+
+    
+}
+}
