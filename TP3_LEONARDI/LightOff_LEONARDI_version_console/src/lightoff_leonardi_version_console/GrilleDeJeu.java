7a495dd40e7f3125b0896192a529e6d5a8702a03
Lucas <lucas.leonardi@epfedu.fr>
2023-10-25 12:59:41 +0200

    PROJET GUIDE

---
+++ TP3_LEONARDI/LightOff_LEONARDI_version_console/src/lightoff_leonardi_version_console/GrilleDeJeu.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_leonardi_version_console;
+
+/**
+ *
+ * @author leona
+ */
+public class GrilleDeJeu {
+    
+   int nbLignes;
+   int nbColonnes;
+   public CelluleLumineuse [][] matricesCellules;
+
+    public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
+        nbLignes = p_nbLignes;
+        nbColonnes = p_nbColonnes;
+        
+    }
+   
+   
+    
+}
